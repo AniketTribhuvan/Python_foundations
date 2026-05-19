c9bf59976c4c25182d71391b201dd5d23578f0a4
AniketTribhuvan <anikettribhuvan07@gmail.com>
2026-05-19 11:35:30 +0530

    Logical and comparison operators

---
+++ Basics/logical_and_comparison_operators.py
@@ -0,0 +1,20 @@
+age = 16
+marks = 85
+
+# Comparison operators
+print(age > 18)      # False
+print(age == 16)     # True
+print(marks != 90)   # True
+
+# Logical operators
+print(age > 15 and marks > 80)   # True
+print(age > 18 or marks > 80)    # True
+print(not(age > 18))             # True
+
+# Output
+# False
+# True
+# True
+# True
+# True
+# True
