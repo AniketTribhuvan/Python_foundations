671c25778c5cda05faf086846570d11a633e6d38
AniketTribhuvan <anikettribhuvan07@gmail.com>
2026-05-20 16:16:54 +0530

    Add arithmetic_operator

---
+++ Basics/arithmetic_operators.py
@@ -0,0 +1,25 @@
+# Taking two numbers as input from the user
+
+n1 = int(input("Enter 1st number = "))
+n2 = int(input("Enter 2nd number = "))
+
+print("The two numbers are =", n1, "&", n2)
+
+# Performing arithmetic operations
+
+print("Addition =", n1 + n2)
+
+print("Subtraction =", n1 - n2)
+
+print("Multiplication =", n1 * n2)
+
+print("Division =", n1 / n2)
+
+# Output
+# Enter 1st number = 10
+# Enter 2nd number = 5
+# The two numbers are = 10 & 5
+# Addition = 15
+# Subtraction = 5
+# Multiplication = 50
+# Division = 2.0
